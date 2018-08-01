1b3297596f5463669bf0498a1fc71a137b0bd782
luis.nicolay <luis.nicolay@LAB1-19-WIN.ROCHAS>
2018-07-31 21:52:47 -0300

    Primeiro Commit

--- ProjetoRPG/src/projetorpg/Personagem.java
+++ ProjetoRPG/src/projetorpg/Personagem.java
@@ -11,4 +11,52 @@ package projetorpg;
  */
 public class Personagem {
     
+    private String nome;
+    private String classe;
+    private int nivel;
+    private int vida;
+    private int ataque;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    
+    
+    
 }
